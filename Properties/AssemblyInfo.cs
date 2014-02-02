34fe9e5466d07754de91b1981c801dbf3dee2a4f
Piedone <piedone@pyrocenter.hu>
2014-02-02 14:47:25 +0100

    Fixing AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lombiq.PrettyGoodBootstrapBaseTheme.Properties")]
+[assembly: AssemblyTitle("Lombiq.PrettyGoodBootstrapBaseTheme")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lombiq.PrettyGoodBootstrapBaseTheme.Properties")]
+[assembly: AssemblyProduct("Lombiq.PrettyGoodBootstrapBaseTheme")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
