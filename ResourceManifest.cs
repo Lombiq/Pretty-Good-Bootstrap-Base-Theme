14de676771dcedb51ce60dd7d7f4691561e94f83
Gábor Domonkos <gabor.domonkos@lombiq.com>
2018-05-19 19:06:18 +0200

    Adding consent banner styling for Lombiq.PrettyGoodBootstrapBaseTheme
    --HG--
    branch : issue/OSOE-22

---
+++ ResourceManifest.cs
@@ -0,0 +1,14 @@
+﻿using Orchard.UI.Resources;
+
+namespace Lombiq.PrettyGoodBootstrapBaseTheme
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+
+            manifest.DefineStyle("Lombiq.PrettyGoodBootstrapBaseTheme.ConsentBanner").SetUrl("consent-banner.min.css", "consent-banner.css");
+        }
+    }
+}
